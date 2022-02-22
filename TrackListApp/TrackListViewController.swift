84bf12011d5ee84f37157c3489972dda289df02c
Nikolay <halemhood@icloud.com>
2022-02-22 23:13:28 +0300

    Full setup app

--- TrackListApp/TrackListViewController.swift
+++ TrackListApp/TrackListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 class TrackListViewController: UITableViewController {
 
     //MARK: - Publick Properties
-    let trackList = ["Rammlied - Rammstein",
+    var trackList = ["Rammlied - Rammstein",
                      "Ich tu dir weh - Rammstein",
                      "Waidmanns Heil - Rammstein",
                      "Haifisch - Rammstein",
@@ -21,6 +21,12 @@ class TrackListViewController: UITableViewController {
                      "Liebe ist für alle da - Rammstein",
                      "Mehr - Rammstein",
                      "Roter Sand - Rammstein"]
+    
+    //MARK: - Lifecycle
+    override func viewDidLoad() {
+        navigationItem.leftBarButtonItem = editButtonItem
+    }
+    
     
     // MARK: - Table view data source
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -38,17 +44,27 @@ class TrackListViewController: UITableViewController {
         content.imageProperties.maximumSize = CGSize(width: 50, height: 50)
         
         cell.contentConfiguration = content
-        
+
         return cell
     }
     
+    
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        
+        let currentTrack = trackList.remove(at: sourceIndexPath.row)
+        trackList.insert(currentTrack, at: destinationIndexPath.row)
+        tableView.reloadData()
+    }
+    
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .none
+    }
+
     // MARK: - Navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let indexPath = tableView.indexPathForSelectedRow {
             let detailVC = segue.destination as! DetailViewController
             detailVC.trackName = trackList[indexPath.row]
         }
-        
     }
-
 }
